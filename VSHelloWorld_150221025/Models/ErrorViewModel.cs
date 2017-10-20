2e764b3ec85a2708fea44dfdb9700f03f91998ea
patixa08 <patixa08@users.noreply.github.com>
2017-10-20 16:53:16 +0100

    Adicionar arquivos de projeto.

---
+++ VSHelloWorld_150221025/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VSHelloWorld_150221025.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
