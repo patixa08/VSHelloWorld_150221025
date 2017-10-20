a55b9aa8fe9e3902c916d675ddf0a2c7bc8ecace
patixa08 <patixa08@users.noreply.github.com>
2017-10-20 16:57:35 +0100

    Comentario de teste

--- VSHelloWorld_150221025/Program.cs
+++ VSHelloWorld_150221025/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace VSHelloWorld_150221025
 {
+    //Comentario de teste 
     public class Program
     {
         public static void Main(string[] args)
